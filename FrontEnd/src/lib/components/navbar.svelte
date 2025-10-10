f7e014d417b02f9fd0bcb8fd3bf6fab5bfce0afc
roDEV0 <214171317+CreVolve-Dev@users.noreply.github.com>
2025-10-09 22:26:14 -0400

    initial commit

---
+++ FrontEnd/src/lib/components/navbar.svelte
@@ -0,0 +1,41 @@
+<script>
+    let name = "Guest"
+</script>
+
+<div class="navbar">
+    <a class="active" href="#home">Home</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+    <a class="account" href="#account">{name}</a>
+</div>
+
+<style>
+    .topnav {
+        background-color: #333;
+        overflow: hidden;
+    }
+
+    .topnav a {
+        float: left;
+        color: #f2f2f2;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        font-size: 17px;
+    }
+
+    .topnav a:hover {
+        background-color: #ddd;
+        color: black;
+    }
+
+    .topnav a.active {
+        background-color: #04AA6D;
+        color: white;
+    }
+
+    .topnav a.account {
+        float: right;
+    }
+</style>
