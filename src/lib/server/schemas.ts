0f1d0ebac77ba7b5f08e7de536893474af9749ab
roDEV0 <romanodev0@gmail.com>
2025-10-13 22:43:08 -0400

    Added form schemas

--- src/lib/server/schemas.ts
+++ src/lib/server/schemas.ts
@@ -15,3 +15,19 @@ export const sessionsTable = pgTable("sessions", {
     userID: integer('userID').references(() => usersTable.id).notNull(),
     expiresAt: timestamp('expiresAt').notNull(),
 });
+
+export const formsTable = pgTable("forms", {
+    id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+    team: integer('team').notNull(),
+    author: integer('author').references(() => usersTable.id).notNull(),
+    formData: integer('formData').references(() => formDataTable.id).notNull(),
+})
+
+export const formDataTable = pgTable("formData", {
+    id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+    driveTrain: text('driveTrain'),
+    driveAbility: text('driveAbility'),
+    malfunctions: text('malfunctions'),
+    other: text('general'),
+    overall: text('overall'),
+})
